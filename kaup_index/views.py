bba364ce7088b263fb4ae4528381e98e1595718c
AtsushiArai <cpa.atsushi.arai@gmail.com>
2022-09-21 15:51:38 +0900

    2022-09-21 calc_kaup_index has changed : add round()

--- kaup_index/views.py
+++ kaup_index/views.py
@@ -10,7 +10,7 @@ def kaup(request):
         posted_weight = int(request.POST["weight"])
         posted_height = int(request.POST["height"])
 
-        calc_kaup_index = ((posted_weight*1000) / posted_height**2) * 10
+        calc_kaup_index = round(((posted_weight*1000) / posted_height**2) * 10,2)
 
         print("体重:",posted_weight)
         print("身長:",posted_height)
