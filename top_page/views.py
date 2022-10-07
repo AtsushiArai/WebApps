534a5dda61af9a136b627638aa301d070af2d214
AtsushiArai <cpa.atsushi.arai@gmail.com>
2022-10-07 11:07:20 +0900

    test

--- top_page/views.py
+++ top_page/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, "top_page/index.html")
+    return render(request, "top_page/indextest.html")
