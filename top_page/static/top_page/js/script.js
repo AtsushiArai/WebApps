1b3ae982051c6b2c549d07fba0533a5d9579fc6a
AtsushiArai <cpa.atsushi.arai@gmail.com>
2022-10-07 16:31:56 +0900

    test

--- top_page/static/top_page/js/script.js
+++ top_page/static/top_page/js/script.js
@@ -8,45 +8,45 @@ $(function() {
         var expanded = false;
     
         var open = function () {
-        var tile = $(this).parent();
-    
-        if (!expanded) {
-            tile.addClass("strips__strip--expanded");
-            // add delay to inner text
-            tileText.css("transition", "all .5s .3s cubic-bezier(0.23, 1, 0.32, 1)");
-            stripClose.addClass("strip__close--show");
-            stripClose.css("transition", "all .6s 1s cubic-bezier(0.23, 1, 0.32, 1)");
-            expanded = true;
-        }
+            var tile = $(this).parent();
+        
+            if (!expanded) {
+                tile.addClass("strips__strip--expanded");
+                // add delay to inner text
+                tileText.css("transition", "all .5s .3s cubic-bezier(0.23, 1, 0.32, 1)");
+                stripClose.addClass("strip__close--show");
+                stripClose.css("transition", "all .6s 1s cubic-bezier(0.23, 1, 0.32, 1)");
+                expanded = true;
+            }
         };
     
         var close = function () {
-        if (expanded) {
-            tile.removeClass("strips__strip--expanded");
-            // remove delay from inner text
-            tileText.css("transition", "all 0.15s 0 cubic-bezier(0.23, 1, 0.32, 1)");
-            stripClose.removeClass("strip__close--show");
-            stripClose.css(
-            "transition",
-            "all 0.2s 0s cubic-bezier(0.23, 1, 0.32, 1)"
-            );
-            expanded = false;
-        }
+            if (expanded) {
+                tile.removeClass("strips__strip--expanded");
+                // remove delay from inner text
+                tileText.css("transition", "all 0.15s 0 cubic-bezier(0.23, 1, 0.32, 1)");
+                stripClose.removeClass("strip__close--show");
+                stripClose.css(
+                "transition",
+                "all 0.2s 0s cubic-bezier(0.23, 1, 0.32, 1)"
+                );
+                expanded = false;
+            }
         };
     
         var bindActions = function () {
-        tileLink.on("click", open);
-        stripClose.on("click", close);
+            tileLink.on("click", open);
+            stripClose.on("click", close);
         };
     
         var init = function () {
-        bindActions();
+            bindActions();
         };
     
         return {
-        init: init
+            init: init
         };
   };
   
-  Expand.init;
+  Expand();
 });
