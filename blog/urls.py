91666cd43068a2e5c7f9771f2ccb87c6cfade41f
AtsushiArai <cpa.atsushi.arai@gmail.com>
2022-11-01 00:02:16 +0900

    update design.

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'blog'
 
 urlpatterns = [
-    path('blog/', views.IndexView.as_view(), name='blog'),
+    # path('blog/', views.IndexView.as_view(), name='blog'),
     path('blog_list/', views.PostListView.as_view(), name='blog_list'),
     path('blog_detail/<int:pk>/', views.PostDetailView.as_view(), name='blog_detail'),
 ]
