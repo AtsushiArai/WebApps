c05d5704fe9a7703e6d034addf891e330a7b2135
AtsushiArai <cpa.atsushi.arai@gmail.com>
2022-11-01 00:07:50 +0900

    minor fix.

--- webapp/sitemaps.py
+++ webapp/sitemaps.py
@@ -7,7 +7,7 @@ class Static_Sitemap(Sitemap):
     changefreq = 'daily'
 
     def items(self):
-        return ['index', 'kaup', 'bmi', 'valuation', 'blog']
+        return ['index', 'kaup', 'bmi', 'valuation', 'blog_list']
 
     def location(self, item):
         return reverse(item)
