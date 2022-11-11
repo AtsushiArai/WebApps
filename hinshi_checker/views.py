4d009bc3960e617c1ba0fc28bb426834d6fe58cf
AtsushiArai <cpa.atsushi.arai@gmail.com>
2022-11-11 14:58:54 +0900

    test

--- hinshi_checker/views.py
+++ hinshi_checker/views.py
@@ -16,8 +16,26 @@ def hinshi_checker(request):
 
         for token in tokenizer.tokenize(posted_text):
             surface = token.surface
-            part_of_speech = token.part_of_speech.split(',')[0]
-            result.append([surface, part_of_speech])
+            part_of_speech_0 = token.part_of_speech.split(',')[0]
+            part_of_speech_1 = token.part_of_speech.split(',')[1]
+            part_of_speech_2 = token.part_of_speech.split(',')[2]
+            part_of_speech_3 = token.part_of_speech.split(',')[3]
+            infl_type = token.infl_type
+            infl_form = token.infl_form
+            base_form = token.base_form
+            reading = token.reading
+
+            result.append([
+                surface,
+                part_of_speech_0,
+                part_of_speech_1,
+                part_of_speech_2,
+                part_of_speech_3,
+                infl_type,
+                infl_form,
+                base_form,
+                reading
+                ])
 
         context = {
             'results': result,
